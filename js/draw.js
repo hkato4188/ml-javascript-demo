db33b77f135f001fc9a77c810a7df5557960d2c7
Hiroki Kato <hirokikato1@gmail.com>
2025-02-27 12:00:18 -0500

    fixes undo method correcting syntactical error in redraw() and creates input form for data collection

--- js/draw.js
+++ js/draw.js
@@ -1,21 +1,25 @@
 const draw={};
 
-draw.path=(ctx,path,color="black")=>{
-    ctx.strokeStyle=color;
-    ctx.lineWidth=3;
-    ctx.beginPath();
-    ctx.moveTo(...path[0]);
-    for(let i=1;i<path.length;i++){
-        ctx.lineTo(...path[i]);
-    }
-    // softens lines for drawing
-    ctx.lineCap='round';
-    ctx.lineJoin='round'
-    ctx.stroke()
-}
-
 draw.paths=(ctx,paths,color="black")=>{
     for(const path of paths){
         draw.path(ctx,path,color);
     }
+}
+
+draw.path=(ctx,path,color="black")=>{
+    ctx.strokeStyle=color;
+    ctx.lineWidth=3;
+    ctx.beginPath();
+    // moveTo takes two args | spread operator unpacks and element 0 has x,y coordinates
+    ctx.moveTo(...path[0]);
+    
+    for(let i=1;i<path.length;i++){
+        ctx.lineTo(...path[i]);
+    }
+    
+    // softens lines for drawing
+    ctx.lineCap='round';
+    ctx.lineJoin='round'
+    
+    ctx.stroke()
 }
--- js/sketchPad.js
+++ js/sketchPad.js
@@ -9,32 +9,19 @@ class SketchPad{
         `;
         container.appendChild(this.canvas);
 
+        const lineBreak = document.createElement("br");
+        container.appendChild(lineBreak)
+        this.undoBtn = document.createElement("button")
+        this.undoBtn.innerText = "↩️";
+        container.appendChild(this.undoBtn)
+
         this.ctx=this.canvas.getContext("2d");
+        
         this.paths=[];
         this.isDrawing=false;
-        
-        // Private method
+        this.#redraw();
+
         this.#addEventListeners();
-    }
-
-    #getMouse=(e)=>{
-        const rect=this.canvas.getBoundingClientRect();
-        return [
-            Math.round(e.clientX-rect.left),
-            Math.round(e.clientY-rect.top)
-        ];
-    }
-
-    #redraw(){
-        this.ctx.clearRect(
-            0,
-            0,
-            this.canvas.width, 
-            this.canvas.heigth
-        );
-        // implement draw utility
-        draw.paths(this.ctx,this.paths);
-        
     }
 
     #addEventListeners(){
@@ -49,11 +36,10 @@ class SketchPad{
                 const mouse = this.#getMouse(e);
                 const lastPath=this.paths[this.paths.length-1];
                 lastPath.push(mouse);
-                console.log(this.paths.length)
                 this.#redraw();
             }
         }
-
+        
         this.canvas.onmouseup=()=>{
             this.isDrawing=false;   
         }
@@ -70,5 +56,27 @@ class SketchPad{
         this.canvas.ontouchend=()=>{
             this.canvas.onmouseup();
         }
+        this.undoBtn.onclick=()=>{
+            this.paths.pop();
+            this.#redraw();
+        }
     }
+
+    #getMouse=(e)=>{
+        const rect=this.canvas.getBoundingClientRect();
+        return [
+            Math.round(e.clientX-rect.left),
+            Math.round(e.clientY-rect.top)
+        ];
+    }
+
+    #redraw(){
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        // implement draw utility
+        draw.paths(this.ctx,this.paths);
+        
+        // toggle undo button functionality
+        (this.paths.length>0) ? this.undoBtn.disabled=false : this.undoBtn.disabled=true;
+    }
+
 }
